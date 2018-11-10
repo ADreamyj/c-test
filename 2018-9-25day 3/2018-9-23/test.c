137a3079bfe90593ce7614071520fa6df6cfe4ab
ADreamyj <861396671@qq.com>
2018-11-10 21:35:58 +0800

    提交代码

---
+++ 2018-9-25day 3/2018-9-23/test.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+int main ()
+//{
+// int i=0;
+// int  count=0;
+// for(i=1;i<=100;i++)
+// {
+//   if(i/10==9)
+//   count++;
+//   if(i%10==9)
+//   count++;
+// }
+// printf("%d\n",count);
+//  return 0;
+//}
+
+//{
+//	int i=0;
+//	double sum =0;
+//	int flag =1;
+//  for(i=0;i<=100;i++);
+//  {
+//	  sum+=flag*1.0/i;
+//	  flag=-flag;
+//  }
+//  printf("%lf\n",sum);
+//   return 0;
+//}
+//
+//
+//{ 
+//	int a[5]={1,3,5,7,9};
+//	int b[5]={2,4,6,8,0};
+//	int c[5]={0};
+//    int i;
+//  for(i=0;i<(sizeof(a))/(sizeof(a[0]));i++)
+//  {
+//	c[i]=a[i];
+//	a[i]=b[i];
+//	b[i]=c[i];
+//  }
+//	 for(i=0;i<(sizeof(a))/(sizeof(a[0]));i++)
+//   {
+//     printf("%d",a[5]);
+//   }
+//	 for(i=0;i<(sizeof(b))/(sizeof(b[0]));i++)
+//   {
+//     printf("%d",b[5]);
+//   }
+//
+//
+//return 0;
+//}
+
+//{
+//	int i=0;
+//	int j=0;
+// int a[3]={1,2,3};
+// int b[3]={4,5,6};
+// int c[3]={0};
+// for(i=0;i<(sizeof(a))/(sizeof(a[0]));i++)
+// {
+//   c[i]=a[i];
+//   a[i]=b[i];
+//   b[i]=c[i];
+// }
+// for(i=0;i<(sizeof(a))/(sizeof(a[0]));i++)
+// {
+//   printf("%d",a[3]);
+// }
+//   printf("\n");
+// for(i=0;i<(sizeof(a))/(sizeof(a[0]));i++)
+//{
+//   printf("%d",b[3]);
+// }
+//
+// return 0;
+//}
+#include<stdio.h>
