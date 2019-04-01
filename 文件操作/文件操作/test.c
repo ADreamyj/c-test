1580efc9ca51ed34c637dd9778e5c4079f8cf759
ADreamyj <861396671@qq.com>
2019-04-01 21:55:23 +0800

    提交代码

---
+++ 文件操作/文件操作/test.c
@@ -0,0 +1,39 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+
+//int main()//验证输入缓冲区
+//{
+//	int ch;
+//	char arr[10] = { 0 };
+//	printf("请输入密码\n");
+//	scanf("%s", arr);
+//	printf("请确认密码（Y/N）\n");
+//	ch = getchar();
+//	while(getchar() != '\n')
+//	{
+//		if (ch == 'Y')
+//		{
+//			printf("y");
+//		}
+//		else
+//		{
+//			printf("n");
+//		}
+//	}
+//	
+//	system("pause");
+//	return 0;
+//}
+
+int main()
+{
+	while (1)
+	{
+		printf("a");
+		Sleep(1);//linux下停止1s
+	}
+	return 0;
+}
---
+++ 文件操作/文件操作/test.cpp
@@ -0,0 +1,24 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()//验证输入缓冲区
+{
+	int ch;
+	char arr[10] = { 0 };
+	printf("请输入密码\n");
+	scanf_s("%s", arr);
+	printf("请确认密码（Y/N）\n");
+	ch = getchar();
+	if (ch == 'Y')
+	{
+		printf("y");
+	}
+	else
+	{
+		printf("n");
+	}
+	system("pause");
+	return 0;
+}
