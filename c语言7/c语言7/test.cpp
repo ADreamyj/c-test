a5a9ec55d0e8da34b3c445e07c333de21d8e298a
ADreamyj <861396671@qq.com>
2019-05-01 17:23:44 +0800

    提交代码

---
+++ c语言7/c语言7/test.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS  1
+
+#include<iostream>
+using namespace std;
+int fun() 
+{ 
+	static int count = 1; 
+	return ++count;
+}
+int main() 
+{
+	int answer;
+	answer = fun() - fun() * fun(); 
+	printf("%d\n", answer);//输出多少？  
+	system("pause");
+	return 0;
+} 
+//int main()
+//{
+//	int i = 0, a = 0, b = 2, c = 3, d = 4;
+//	i = ++b || a++ || d++;
+//	cout << i<<" ";
+//	system("pause");
+//	return 0;
+//}
