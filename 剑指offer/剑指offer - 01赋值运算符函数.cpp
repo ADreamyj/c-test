c5e9e586108ba758882e4e44020588f23fac2d5f
ADreamyj <861396671@qq.com>
2019-07-30 23:45:26 +0800

    01

---
+++ 剑指offer/剑指offer - 01赋值运算符函数.cpp
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS  1
+//
+//#include<iostream>
+//#include<string>
+//class CMystring
+//{
+//public:
+//	CMystring(char* pData = nullptr);
+//	CMystring(const CMystring& str);
+//	CMystring& operator = (const CMystring& str);
+//	~CMystring();
+//	
+//private:
+//	char* m_pData;
+//};
+//
+//CMystring::CMystring(char* pData)
+//{
+//
+//}
+//CMystring& CMystring::operator =(const CMystring& str)
+//{
+//	if (this == &str)
+//		return *this;
+//
+//	delete[] m_pData;
+//	m_pData = nullptr;
+//	m_pData = new char[strlen(str.m_pData) + 1];
+//	strcpy(m_pData, str.m_pData);
+//
+//	return *this;
+//}
+
+
